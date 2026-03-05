661821bd9e2b20633254a4645e0a1b155a73cb5d
Christian Albrechtsen <christian.albrechtsen75@gmail.com>
2026-03-05 19:49:22 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime, timezone
+import os
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///blog.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    def __repr__(self):
+        return f"<Post {self.id} {self.title!r}>"
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(120), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # Relationship to Post
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    post = db.relationship('Post', backref=db.backref('comments', cascade='all, delete-orphan', lazy='dynamic'))
+
+    def __repr__(self):
+        return f"<Comment {self.id} on Post {self.post_id!r}>"
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/post/new", methods=["GET", "POST"])
+def new_post():
+    return render_template("new_post.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
